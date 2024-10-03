8dffd72d2f437e595e7359697bce5943010a9897
DariusEmanuel <dariusciuverca@gmail.com>
2024-10-04 02:15:26 +0300

    [WIP] Create task definition loading mechanism
    Fixes #14

---
+++ cli/dev/defs1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using YamlPrompt.Model;
+
+namespace defs1;
+
+public class Class1 : TaskDefinitionBase<IReadOnlyDictionary<string, object?>>
+{
+	public override string TypeKey => "def1";
+
+	public override IReadOnlyDictionary<string, object?> InterpretPayload(
+		IReadOnlyDictionary<string, object?> fields)
+	{
+		return fields;
+	}
+
+	protected override string? Invoke(
+		AutomationContext context,
+		IReadOnlyDictionary<string, object?> payload,
+		string? previousResult)
+	{
+		Console.WriteLine(((object[])payload["def1"])[0]);
+		Console.WriteLine("Previous result: " + previousResult);
+		return "abc from def1";
+	}
+}
--- cli/src/Program.cs
+++ cli/src/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.CommandLine;
 using YamlPrompt.Api;
 using YamlPrompt.Api.Serialization;
-using YamlPrompt.Model;
+using YamlPrompt.Cli.Registry;
 
 var yamlFileArgument = new Argument<string>("yamlFile", description: "The YAML input string");
 
@@ -15,31 +15,11 @@ rootCommand.SetHandler(static (string yamlFile) =>
 {
     var yamlInput = File.ReadAllText(yamlFile);
     var deserializer = new AutomationScriptDeserializer();
-    var definitions = new[] { new FakeShellTaskDefinition() };
-    var automationScript = deserializer.DeserializeYaml(yamlInput, definitions.Select(d => d.TypeKey).ToArray());
+    var definitions = DefinitionsLoader.LoadTaskDefinitions();
+    var knownTypes = definitions.Select(d => d.TypeKey).ToArray();
+    var automationScript = deserializer.DeserializeYaml(yamlInput, knownTypes);
 
     AutomationScript.Run(automationScript, definitions);
 }, yamlFileArgument);
 
 return await rootCommand.InvokeAsync(args);
-
-class FakeShellTaskDefinition : TaskDefinitionBase<IReadOnlyDictionary<string, object?>>
-{
-    public override string TypeKey => "shell";
-
-    public override IReadOnlyDictionary<string, object?> InterpretPayload(
-		IReadOnlyDictionary<string, object?> fields)
-    {
-        return fields;
-    }
-
-    protected override string? Invoke(
-		AutomationContext context,
-		IReadOnlyDictionary<string,
-		object?> payload,
-		string? previousResult)
-    {
-        Console.WriteLine(((object[])payload["shell"])[0]);
-		    return "ABC";
-    }
-}
